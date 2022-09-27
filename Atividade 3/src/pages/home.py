212c4fae9ce8a2424cfdf06832980c7866c6b4ec
Anah <ana.marcacini2000@gmail.com>
2022-09-26 22:30:34 -0300

    home normal

--- Atividade 3/src/pages/home.py
+++ Atividade 3/src/pages/home.py
@@ -1,11 +1,23 @@
 import streamlit as st
 
+
+main, info, carrinho,sair = st.tabs(["Home", "Info", "Carrinho 🛒","Sair"])
+with main:
+        st.title("Home")
+        imagensBanner = ["assets/B1.jpg","assets/B2.jpg"]
+        st.image(   
+                image=imagensBanner,
+                #caption=""
+            )
+
+
 def AbrirHome(a):
     main, info, carrinho,sair = st.tabs(["Home", "Info", "Carrinho 🛒","Sair"])
-    st.title("Home")
-    imagensBanner = ["assets/B1.jpg","assets/B2.jpg"]
-    st.image(   
-            image=imagensBanner,
-            #caption=""
-        )
+    with main:
+        st.title("Home")
+        imagensBanner = ["assets/B1.jpg","assets/B2.jpg"]
+        st.image(   
+                image=imagensBanner,
+                #caption=""
+            )
 
