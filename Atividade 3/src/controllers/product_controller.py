7d46d9bec3fb0596030e466a6f2e526139f957a7
Anah <ana.marcacini2000@gmail.com>
2022-10-07 16:02:47 -0300

    -

---
+++ Atividade 3/src/controllers/product_controller.py
@@ -0,0 +1,30 @@
+from models.product import Product
+
+
+class UserController():
+    def __init__(self) -> None:
+        #Carrega os dados dos usuários
+        self.produtos = [#estoque
+            Product("Produto 1", "100,00"),
+            Product("Produto 2", "110,00"),
+            Product("Produto 3", "120,00"),
+            Product("Produto 4", "130,00"),
+            Product("Produto 5", "140,00"),
+            Product("Produto 6", "150,00"),
+            Product("Produto 7", "160,00"),
+            Product("Produto 8", "170,00")
+        ]
+
+        self.carrinho = []
+
+    '''
+    def checkUser(self,user):
+        return user in self.users
+
+    def checkLogin(self, name, password):
+        user_teste = User(name=name, password=password, email=None)
+        for user in self.users:
+            if user.name == user_teste.name and user.password == user_teste.password:
+                return True
+        return False
+'''
