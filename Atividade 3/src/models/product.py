f97a650231ed19e80d7b3956ec6a57797b75807c
Anah <ana.marcacini2000@gmail.com>
2022-10-11 15:13:15 -0300

    .

--- Atividade 3/src/models/product.py
+++ Atividade 3/src/models/product.py
@@ -2,11 +2,10 @@ class Product():
     def __init__(self, name, price) -> None:
         self.name = name
         self.price = price
-        #self.url = url
+        #self.url = url futuro
     def __str__(self) -> str:
         return f'{self.name}--- preço :{self.price})'
 
 
     def getPreco(self):
         return self.price
-        
