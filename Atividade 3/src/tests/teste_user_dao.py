bdd6baa0477271f895c9676efa4affee1150fa1a
Anah <ana.marcacini2000@gmail.com>
2022-11-03 15:36:43 -0300

    testes

---
+++ Atividade 3/src/tests/teste_user_dao.py
@@ -0,0 +1,51 @@
+from src.dao.usuario_dao import UsuarioDao
+from src.models.user import User
+
+
+
+##Ver todos os usuarios
+
+usuarios = UsuarioDao.get_instance().get_all()
+
+for usuario in usuarios:
+    print(usuarios)
+
+print(50*"#")
+
+
+
+
+user = User(email="souNovo@gmail.com",name = "AcabeiDeChegar", password= "password")
+UsuarioDao.get_instance().inserir_usuario(user)
+
+usuarios = UsuarioDao.get_instance().get_all()
+for usuario in usuarios:
+    print(usuarios)
+
+print(50*"#")
+
+
+
+
+
+usuario = UsuarioDao.get_instance().pegar_item(user.email)
+usuario.nome = "EstouVelho"
+usuario.password = "senha"
+print(UsuarioDao.get_instance().atualizar_item(usuario))
+
+usuarios = UsuarioDao.get_instance().get_all()
+for usuario in usuarios:
+    print(usuarios)
+
+print(50*"#")
+
+
+
+
+print(UsuarioDao.get_instance().deletar_item(usuario.email))
+
+print(50*"#")
+
+usuarios = UsuarioDao.get_instance().get_all()
+for usuario in usuarios:
+    print(usuarios)
